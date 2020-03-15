22cfd44c3782979f970cf4fe49c8249fff341947
Jay Harris <jay.harris@outlook.co.nz>
2020-03-15 11:38:40 +1100

    Improves tile loader speed

--- src/utils/tileDownloader.js
+++ src/utils/tileDownloader.js
@@ -3,13 +3,14 @@ import { latLngToTile } from './slippyCoords';
 const ESTIMATED_TILE_SIZE = 40 * 1024; // Estimate each tile at 40kb.
 
 export class TileDownloader {
-    constructor(bounds, startZoom, maxZoom = 17) {
+    constructor(bounds, startZoom, maxZoom = 17, numWorkers = 50) {
         this.min = bounds.getNorthWest();
         this.max = bounds.getSouthEast();
         this.startZoom = Math.floor(startZoom);
 
         // Max zoom should be at most 15, unless we're super zoomed in, then at most 17.
         this.maxZoom = Math.min(17, Math.max(this.startZoom + 3, 15));
+        this.numWorkers = numWorkers;
     }
 
     numTiles() {
@@ -33,35 +34,48 @@ export class TileDownloader {
 
     *tiles(zoom) {
         if (zoom === undefined)
-          zoom = this.startZoom;
+            zoom = this.startZoom;
 
         const { x: minX, y: minY } = latLngToTile(this.min, zoom);
         const { x: maxX, y: maxY } = latLngToTile(this.max, zoom);
 
         for (let x = minX; x <= maxX; ++x)
-          for  (let y = minY; y <= maxY; ++y)
-            yield { x, y, z: zoom };
+            for (let y = minY; y <= maxY; ++y)
+                yield { x, y, z: zoom };
 
         if (zoom < this.maxZoom)
-          yield* this.tiles(zoom + 1);
+            yield* this.tiles(zoom + 1);
     }
 
     async downloadTiles(tileUrl, onProgress) {
-        onProgress = onProgress || (() => {});
-        
+        onProgress = onProgress || (() => { });
+
         const servers = "abc";
         const totalTiles = this.numTiles();
         let downloadedTiles = 0;
 
-        for (const tile of this.tiles()) {
-            const url = tileUrl
-                .replace("{s}", servers[downloadedTiles++ % servers.length])
-                .replace("{x}", tile.x)
-                .replace("{y}", tile.y)
-                .replace("{z}", tile.z);
-            await fetch(url);
+        const queue = this.tiles();
 
-            onProgress(downloadedTiles/totalTiles);
+        const worker = async () => {
+            let result = queue.next();
+            while (!result.done) {
+                const tile = result.value;
+                const url = tileUrl
+                    .replace("{s}", servers[downloadedTiles++ % servers.length])
+                    .replace("{x}", tile.x)
+                    .replace("{y}", tile.y)
+                    .replace("{z}", tile.z);
+                await fetch(url);
+
+                onProgress(downloadedTiles / totalTiles);
+                result = queue.next();
+            }
         }
+
+        const workers = [];
+        for (let i = 0; i < this.numWorkers; ++i)
+            workers.push(worker());
+
+        await Promise.all(workers);
     }
 }
