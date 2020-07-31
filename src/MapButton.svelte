4e0bafe252e4382dd22863ae088bc03b5deb9d74
Jay Harris <jay.harris@outlook.co.nz>
2020-07-31 14:25:25 +1200

    WIP buttons (open layer buttons suck

---
+++ src/MapButton.svelte
@@ -0,0 +1,33 @@
+<script lang="ts">
+  import { Control } from "ol/control";
+  import { CLASS_CONTROL, CLASS_UNSELECTABLE } from "ol/css";
+  import type { Map } from "ol";
+
+  class Button extends Control {
+    labelNode: Text;
+    button: HTMLButtonElement;
+
+    constructor(props: { label: string }) {
+      super({
+        element: document.createElement("div"),
+      });
+
+      this.labelNode = document.createTextNode(props.label);
+      this.button = document.createElement("button");
+
+      this.button.appendChild(this.labelNode);
+      this.element.appendChild(this.button);
+      this.element.classList.add(CLASS_CONTROL, CLASS_UNSELECTABLE);
+    }
+  }
+
+  export let map: Map;
+
+  let content: HTMLElement;
+
+  const button = new Button({ label: "⬊" });
+
+  $: if (map) map.addControl(button);
+</script>
+
+<div id="foobar" bind:this={content} />
--- src/MapLocator.svelte
+++ src/MapLocator.svelte
@@ -1,28 +1,29 @@
 <script lang="ts">
   import type { Map } from "ol";
   import Geolocation from "ol/Geolocation";
-  import Feature from 'ol/Feature';
-  import VectorSource from 'ol/source/Vector';
-  import VectorLayer from 'ol/layer/Vector';
-  import Point from 'ol/geom/Point';
-import Style from "ol/style/Style";
-import Icon from "ol/style/Icon";
+  import Feature from "ol/Feature";
+  import VectorSource from "ol/source/Vector";
+  import VectorLayer from "ol/layer/Vector";
+  import Point from "ol/geom/Point";
+  import Style from "ol/style/Style";
+  import Icon from "ol/style/Icon";
+  import MapButton from "./MapButton.svelte";
 
   export let map: Map;
 
   let tracking = true;
   let geolocation: Geolocation;
   let vectorSource = new VectorSource({
-      features: []
+    features: [],
   });
   let vectorLayer = new VectorLayer({
-    source: vectorSource
+    source: vectorSource,
   });
 
   $: {
-      if (map){
-        map.addLayer(vectorLayer);
-      }
+    if (map) {
+      map.addLayer(vectorLayer);
+    }
   }
 
   $: {
@@ -33,26 +34,26 @@ import Icon from "ol/style/Icon";
       });
 
       // When we first get the location, show it.
-      geolocation.once('change', e => {
+      geolocation.once("change", (e) => {
         const position = geolocation.getPosition();
         map.getView().setCenter(position);
-      })
+      });
 
       geolocation.on("change", (e) => {
         const position = geolocation.getPosition();
         const iconFeature = new Feature({
-            geometry: new Point(position),       
+          geometry: new Point(position),
         });
 
         const iconStyle = new Style({
-            image: new Icon({
-                anchor: [0.5, 0.5],
-                opacity: 0.9,
-                imgSize: [600, 600],
-                scale: 0.08,
-                color: '#578dfF',
-                src: 'location-indicator.svg',
-            })
+          image: new Icon({
+            anchor: [0.5, 0.5],
+            opacity: 0.9,
+            imgSize: [600, 600],
+            scale: 0.08,
+            color: "#578dfF",
+            src: "location-indicator.svg",
+          }),
         });
         iconFeature.setStyle(iconStyle);
 
@@ -61,8 +62,9 @@ import Icon from "ol/style/Icon";
       });
 
       // If we aren't tracking the location, don't show it.
-      if (!tracking)
-        vectorSource.clear();
+      if (!tracking) vectorSource.clear();
     }
   }
 </script>
+
+<MapButton {map}/>
