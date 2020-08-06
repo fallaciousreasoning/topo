4218501adbdb76d6c658755c410e050c27d9373a
Jay Harris <jay.harris@outlook.co.nz>
2020-08-06 19:04:13 +1200

    Remove layers, to test

--- src/TopoMap.svelte
+++ src/TopoMap.svelte
@@ -42,20 +42,20 @@ import portal from './utils/portal'
         new LayerGroup({
           title: "Base Layers",
           layers: [
-            new TileLayer({
-              title: "NZ Topo",
-              type: "base",
-              source: new XYZ({
-                url:
-                  "https://tiles-{a-c}.data-cdn.linz.govt.nz/services;key=d0772bed2204423f87157f7fb1223389/tiles/v4/layer=50767/EPSG:3857/{z}/{x}/{y}.png",
-              }),
-            } as any),
-            new TileLayer({
-              title: "Open Street Maps",
-              type: "base",
-              visible: false,
-              source: new OSM(),
-            } as any),
+            // new TileLayer({
+            //   title: "NZ Topo",
+            //   type: "base",
+            //   source: new XYZ({
+            //     url:
+            //       "https://tiles-{a-c}.data-cdn.linz.govt.nz/services;key=d0772bed2204423f87157f7fb1223389/tiles/v4/layer=50767/EPSG:3857/{z}/{x}/{y}.png",
+            //   }),
+            // } as any),
+            // new TileLayer({
+            //   title: "Open Street Maps",
+            //   type: "base",
+            //   visible: false,
+            //   source: new OSM(),
+            // } as any),
           ],
         } as any),
       ],
