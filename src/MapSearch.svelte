dba01e80b14956a33b946512f41e2f3d0c8a4b3e
Jay Harris <jay.harris@outlook.co.nz>
2020-08-06 19:36:17 +1200

    Begins work on custom geocoder

--- src/MapSearch.svelte
+++ src/MapSearch.svelte
@@ -1,18 +1,36 @@
 <script lang="ts">
- import * as ol from 'ol';
-//   import * as Geocoder from "ol-geocoder/dist/ol-geocoder-debug";
-  import "ol-geocoder/dist/ol-geocoder.css";
-//   console.log(Geocoder)
   import type { Map } from "ol";
+  import MapButton from "./MapButton.svelte";
 
-//   export let map: Map;
-//   const geocoder = new Geocoder("nominatim", {
-//     provider: "mapquest",
-//     lang: "en-NZ",
-//     placeholder: "Search for ...",
-//     targetType: "text-input",
-//     limit: 5,
-//     keepOpen: true,
-//   });
-//   $: map && map.addControl(geocoder);
+  let searching = false;
 </script>
+
+<style>
+  .search-bar {
+    display: flex;
+    gap: 0.5em;
+  }
+
+  .search-box {
+    flex: 1;
+  }
+</style>
+
+<MapButton top="6.75em">
+  {#if !searching}
+    <button on:click={() => (searching = true)}>🔎</button>
+  {:else}
+    <div>
+      <div class="search-bar">
+        <input class="search-box" type="search" />
+        <button class="clear-button" on:click={() => (searching = false)}>
+          X
+        </button>
+      </div>
+      <div classname="results">
+        <div>Result 1</div>
+        <div>Result 2</div>
+      </div>
+    </div>
+  {/if}
+</MapButton>
