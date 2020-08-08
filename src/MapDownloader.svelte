95ad3c7a3546f9ce057b4ee9279db3ae5d8560b2
Jay Harris <jay.harris@outlook.co.nz>
2020-08-08 18:41:25 +1200

    Only show download button when zoomed in

--- src/MapDownloader.svelte
+++ src/MapDownloader.svelte
@@ -2,22 +2,39 @@
   import Control from "./ol/Control.svelte";
   import { getOlContext } from "./ol/Map.svelte";
   import { TileDownloader } from "./utils/tileDownloader";
-import { tileUrlFunction } from "./layers/linzTopoSource";
+  import { tileUrlFunction } from "./layers/linzTopoSource";
+  import { friendlyBytes } from "./utils/bytes";
+  import { onMount } from "svelte";
+  import onMountTick from "./utils/onMountTick";
+
   const { getMap } = getOlContext();
+
+  let zoom = 11;
+
+  onMountTick(() => {
+    zoom = getMap().getView().getZoom();
+
+    const handler = () => {
+      zoom = getMap().getView().getZoom();
+    };
+    getMap().on("moveend", handler);
+    return () => getMap().un("moveend", handler);
+  });
 </script>
 
-<Control control>
-  <button
-    on:click={async () => {
-      const view = getMap().getView();
-      const extent = view.calculateExtent();
-      const downloader = new TileDownloader(extent, view.getZoom());
-
-      const message = `Would you like to download tiles?\nThis will use approximately ${downloader.estimatedSize()} bytes of storage.`
-      if (!window.confirm(message)) return;
-
-      await downloader.downloadTiles(tileUrlFunction, console.log);
-    }}>
-    ⤓
-  </button>
-</Control>
+{#if zoom >= 11}
+  <Control control>
+    <button
+      on:click={async () => {
+        const view = getMap().getView();
+        const extent = view.calculateExtent();
+        const downloader = new TileDownloader(extent, view.getZoom());
+        const message = `Would you like to download tiles?\nThis will use approximately ${friendlyBytes(downloader.estimatedSize())} of storage.`;
+        if (!window.confirm(message)) return;
+        await downloader.downloadTiles(tileUrlFunction, console.log);
+        alert('Downloaded tiles!');
+      }}>
+      ⤓
+    </button>
+  </Control>
+{/if}
