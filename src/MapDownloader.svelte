f82ccccf85a18505ed7fa176bd9205e830c1b803
Jay Harris <jay.harris@outlook.co.nz>
2020-08-08 18:48:42 +1200

    Begins work on spinner

--- src/MapDownloader.svelte
+++ src/MapDownloader.svelte
@@ -6,10 +6,12 @@
   import { friendlyBytes } from "./utils/bytes";
   import { onMount } from "svelte";
   import onMountTick from "./utils/onMountTick";
+  import Spinner from "./components/Spinner.svelte";
 
   const { getMap } = getOlContext();
 
   let zoom = 11;
+  let downloading = false;
 
   onMountTick(() => {
     zoom = getMap().getView().getZoom();
@@ -22,19 +24,25 @@
   });
 </script>
 
-{#if zoom >= 11}
+{#if zoom >= 11 || downloading}
   <Control control>
-    <button
-      on:click={async () => {
-        const view = getMap().getView();
-        const extent = view.calculateExtent();
-        const downloader = new TileDownloader(extent, view.getZoom());
-        const message = `Would you like to download tiles?\nThis will use approximately ${friendlyBytes(downloader.estimatedSize())} of storage.`;
-        if (!window.confirm(message)) return;
-        await downloader.downloadTiles(tileUrlFunction, console.log);
-        alert('Downloaded tiles!');
-      }}>
-      ⤓
-    </button>
+    {#if !downloading}
+      <button
+        on:click={async () => {
+          const view = getMap().getView();
+          const extent = view.calculateExtent();
+          const downloader = new TileDownloader(extent, view.getZoom());
+          const message = `Would you like to download tiles?\nThis will use approximately ${friendlyBytes(downloader.estimatedSize())} of storage.`;
+          if (!window.confirm(message)) return;
+          downloading = true;
+          await downloader.downloadTiles(tileUrlFunction, console.log);
+          downloading = false;
+          alert('Downloaded tiles!');
+        }}>
+        ⤓
+      </button>
+    {:else}
+      <Spinner />
+    {/if}
   </Control>
 {/if}
