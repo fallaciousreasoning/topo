b22791c436f5c322d4b04ba4db6c6be9535e265e
Jay Harris <jay.harris@outlook.co.nz>
2020-08-08 18:25:57 +1200

    Load LINZ tiles over https

--- src/layers/linzTopoSource.ts
+++ src/layers/linzTopoSource.ts
@@ -9,7 +9,7 @@ export const tileUrlFunction = (tile: TileCoord): string => {
 
 export const tileUrl = ([z,x,y]: TileCoord, source: string) => {
     const layer = z < 13 ? '50798' : '50767';
-    return `http://tiles-${source}.data-cdn.linz.govt.nz/services;key=d0772bed2204423f87157f7fb1223389/tiles/v4/layer=${layer}/EPSG:3857/${z}/${x}/${y}.png`;
+    return `https://tiles-${source}.data-cdn.linz.govt.nz/services;key=d0772bed2204423f87157f7fb1223389/tiles/v4/layer=${layer}/EPSG:3857/${z}/${x}/${y}.png`;
 }
 
 export const tileCacheId = (tile: TileCoord) => tileUrl(tile, "x");
