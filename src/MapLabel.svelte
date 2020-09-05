379ce1f07f373e732cf1af88573e3c449a5205b4
Jay Harris <jay.harris@outlook.co.nz>
2020-09-06 11:06:46 +1200

    Better positioning

--- src/MapLabel.svelte
+++ src/MapLabel.svelte
@@ -1,15 +1,25 @@
 <script lang="ts">
   import { fromLonLat } from "ol/proj";
+  import { getOlContext } from "./ol/Map.svelte";
 
   import Popup from "./ol/Popup.svelte";
-  import fragment from './stores/fragment';
+  import fragment from "./stores/fragment";
+
+  const { getMap } = getOlContext();
 
   $: labelInfo = $fragment.label;
-  $: position = fromLonLat([labelInfo.lng, labelInfo.lat])
+  $: position = fromLonLat([labelInfo.lng, labelInfo.lat]);
+
+  $: {
+    const map = getMap();
+    if (map && labelInfo.text) {
+      map.getView().animate({ center: position });
+    }
+  }
 </script>
 
 {#if !!labelInfo.text}
-  <Popup {position} autoPan>
+  <Popup {position}>
     <p>{labelInfo.text}</p>
   </Popup>
 {/if}
