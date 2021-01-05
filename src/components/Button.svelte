79157a4789f411983c0f79cd43d32d2a9e52e164
Jay Harris <jay.harris@outlook.co.nz>
2021-01-06 05:28:11 +1300

    Add button

---
+++ src/components/Button.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+    let clazz;
+    export { clazz as class };
+</script>
+
+<button
+    on:click
+    class={`z-10 py-2 px-4 text-sm font-medium relative overflow-hidden text-white duration-200 ease-in bg-purple-500 hover:bg-purple-400 hover:elevation-5 elevation-3 rounded button ${clazz}`}>
+    <slot/>
+</button>
