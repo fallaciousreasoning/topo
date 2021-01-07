2965b474b814ffe6adf64b7b8eb449b29b53d9b5
Jay Harris <jay.harris@outlook.co.nz>
2021-01-08 09:50:35 +1300

    Fancy styles

--- src/components/MapSearch.svelte
+++ src/components/MapSearch.svelte
@@ -6,6 +6,7 @@
   import { getOlContext } from "../ol/Map.svelte";
   import MapControl from "./MapControl.svelte";
   import grow from "../transitions/grow";
+import { debounce } from "../utils/debounce";
 
   const dispatcher = createEventDispatcher();
   const { map } = getOlContext();
@@ -18,7 +19,7 @@
   }
 
   let query = "";
-  let searching = false;
+  let open = false;
   let results = [];
 
   let shouldClose = false;
@@ -26,19 +27,23 @@
     shouldClose = true;
     setTimeout(() => {
       if (!shouldClose) return;
-      searching = false;
+      open = false;
     });
   }
 
+  let searching = false;
   const updateResults = async () => {
     try {
+      searching = true;
       results = await geocode(query);
+      searching = false;
     } catch {}
   };
+  const debouncedUpdateResults = debounce(updateResults, 500);
 
   const selectResult = (result: GeocodeResult) => {
     dispatcher("change", { result, map });
-    searching = false;
+    open = false;
   };
 </script>
 
@@ -47,32 +52,32 @@
     tabindex=0
     on:focusin={(e) => {
       shouldClose = false;
-      searching = true;
+      open = true;
     }}
     on:focusout={close}>
     <div
-      class="transition-colors divide-purple-200 flex rounded items-start border ${searching && 'focus-within:border-primary focus-within:ring-2 focus-within:ring-primary'}">
+      class="transition-colors divide-purple-200 flex rounded items-start border ${open && 'focus-within:border-primary focus-within:ring-2 focus-within:ring-primary'}">
       <button
         tabindex=-1
-        class={`transition-colors duration-200 map-button flex-shrink-0 ${searching && 'bg-primary focus:bg-primary-hover hover:bg-primary-hover'}`}>
+        class={`transition-colors duration-200 map-button flex-shrink-0 ${open && 'bg-primary focus:bg-primary-hover hover:bg-primary-hover'}`}>
         <span class="-mx-2">🔎</span>
       </button>
 
-      {#if searching}
+      {#if open}
         <input
           bind:this={searchBox}
           transition:grow={{ animateHeight: false }}
-          class={`${!searching && 'hidden'} h-full py-2 px-2 rounded-l-none appearance-none rounded focus:outline-none`}
+          class={`${!open && 'hidden'} h-full py-2 px-2 rounded-l-none appearance-none rounded focus:outline-none`}
           type="search"
           bind:value={query}
-          on:change={updateResults} />
+          on:keydown={debouncedUpdateResults} />
       {/if}
     </div>
-    {#if searching}
+    {#if open}
       <div class={`max-h-72 overflow-y-auto ${!!results.length && 'mt-1'}`}>
-        {#each results as result}
+        {#each results.slice(0, 20) as result}
           <div
-            class="opacity-95 hover:bg-background-hover"
+            class="opacity-95 hover:bg-background-hover px-2 py-1"
             on:click={() => selectResult(result)}>
             {result.name}
           </div>
