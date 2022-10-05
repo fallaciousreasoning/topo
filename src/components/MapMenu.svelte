d2c82fe5d6c1504edf544332047154a4acc38307
Jay Harris <jay.harris@outlook.co.nz>
2022-10-05 16:09:41 +1300

    Add menu button

---
+++ src/components/MapMenu.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+  import MapControl from './MapControl.svelte'
+  import fragment from '../stores/fragment'
+
+  const toggleMenu = () => {
+    if ($fragment.page === 'settings') {
+      history.go(-1)
+    } else {
+      fragment.update((p) => ({ ...p, page: 'settings' }))
+    }
+  }
+</script>
+
+<MapControl>
+  <div class="flex flex-col">
+    <button class="map-button rounded-b-none" on:click={(e) => toggleMenu()}
+      >☰</button
+    >
+  </div>
+</MapControl>
