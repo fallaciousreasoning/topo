c03fecf5a896a02328d71e58726011156bf08cd8
Jay Harris <jay.harris@outlook.co.nz>
2022-10-05 17:07:20 +1300

    Cleanup settings

--- src/components/SettingsView.svelte
+++ src/components/SettingsView.svelte
@@ -5,6 +5,7 @@ import settings from '../stores/settings';
 import Section from "./Section.svelte";
 import Checkbox from './Checkbox.svelte';
 import { onMount } from "svelte";
+    import Card from "./Card.svelte"
 
 onMount(() => {
     for (const layer of layerDefinitions) {
@@ -20,22 +21,18 @@ onMount(() => {
 <Section page="settings" title="Settings">
 	<div class="w-full border-t border-foreground mt-1" />
 	<div>
-		<h4 class="mt-2 font-semibold text-base">Appearance</h4>
-		<div>Theme Color</div>
-		<div> Background Color</div>
-	</div>
-	<div class="w-full border-t border-foreground mt-1" />
-	<div>
 		<h4 class="mt-2 font-semibold text-base">Layers</h4>
+		<div class="flex flex-col gap-2 my-1">
 		{#each layerDefinitions.filter(l => l.type === "base") as layer}
-			<div class="my-2">
+			<Card>
 				<h5 class="font-semibold">{layer.name}</h5>
 				<div class="text-gray-500 italic">{layer.description}</div>
 				<Checkbox label="Cache Viewed Tiles"
 					checked={$settings.baseLayers[layer.name].cache}
 					on:change={e => settings.updateBaseLayer(layer.name, { cache: e.target['checked'] })}/>
 				<div>Currently using 0 bytes of storage.</div>
-			</div>
+			</Card>
 		{/each}
+	</div>
 	</div>
 </Section>
--- src/components/TracksView.svelte
+++ src/components/TracksView.svelte
@@ -3,27 +3,30 @@
   import { liveQuery } from 'dexie'
   import { db, updateItem } from '../db'
     import Button from './Button.svelte'
+    import Card from './Card.svelte'
 
   const tracks = liveQuery(() => db.tracks.toArray())
 </script>
 
 <Section page="tracks" title="Tracks">
   <div class="w-full border-t border-foreground mt-1" />
-  <div>
+  <div class="my-2">
     {#if $tracks}
       {#each $tracks as track}
-        <div class="bg-white flex flex-col p-2 my-1 gap-1 rounded shadow">
-          <span><span class="font-semibold">Id:</span> {track.id}</span>
-          <span>
-            <span class="font-semibold">Name:</span>
-            <input value={track.name} on:input={e => updateItem('tracks', track.id, { name: e.target['value'] })}/>
-          </span>
-          <span><span class="font-semibold">Points:</span> {track.points.length}</span>
-          <div class="flex flex-row gap-2">
-            <Button class='' on:click={e => window.confirm('Are you sure?') && db.tracks.delete(track.id)}>🗑</Button>
-            <Button class=''>✎</Button>
-          </div>
+        <Card>
+          <div class="flex flex-col gap-1">
+            <span><span class="font-semibold">Id:</span> {track.id}</span>
+            <span>
+                <span class="font-semibold">Name:</span>
+                <input value={track.name} on:input={e => updateItem('tracks', track.id, { name: e.target['value'] })}/>
+            </span>
+            <span><span class="font-semibold">Points:</span> {track.points.length}</span>
+            <div class="flex flex-row gap-2">
+                <Button class='' on:click={e => window.confirm('Are you sure?') && db.tracks.delete(track.id)}>🗑</Button>
+                <Button class=''>✎</Button>
+            </div>
         </div>
+        </Card>
       {/each}
     {/if}
     <div />
