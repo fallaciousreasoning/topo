c118ce3b1cecb96eef8b15f166bd5d58b59b2894
Jay Harris <jay.harris@outlook.co.nz>
2023-02-23 18:29:45 +1300

    Add basic has grade filter

--- src/components/mountains/MountainsFilter.svelte
+++ src/components/mountains/MountainsFilter.svelte
@@ -3,13 +3,30 @@
   import mountains from '../../stores/mountains'
 
   let search: string = ''
-  $: filteredMountains = Object.values($mountains)
-    .filter(m => m.name.toLowerCase().includes(search.toLowerCase()))
+  let hasGrade: number;
+  $: filteredMountains = Object.values($mountains).filter((m) =>
+    m.name.toLowerCase().includes(search.toLowerCase())
+  ).filter(p => !hasGrade || p.routes.some(r => r.grade?.includes(hasGrade))).sort((a, b) => a.name.localeCompare(b.name))
 </script>
 
 <h1 class="font-bold">Mountains</h1>
-<input class="w-full border-black border-solid border p-2 rounded" type="text" bind:value={search} />
-
+<input
+  class="w-full border-black border-solid border p-2 rounded"
+  type="text"
+  bind:value={search} />
+<div class="flex flex-row justify-between w-full">
+  <label>
+    Has Route at Grade
+    <select class="w-full" bind:value={hasGrade}>
+      <option value="">Any</option>
+      <option value="1">1</option>
+      <option value="2">2</option>
+      <option value="3">3</option>
+      <option value="4">4</option>
+      <option value="5">5</option>
+    </select>
+  </label>
+</div>
 {#each filteredMountains as mountain}
   <div>
     <a
