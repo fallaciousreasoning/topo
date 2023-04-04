4232720eb6707ae908e7c80934b983a1b888b4c3
Jay Harris <jay.harris@outlook.co.nz>
2023-04-04 16:47:51 +1200

    Fix bug with null text

--- src/components/TopoText.svelte
+++ src/components/TopoText.svelte
@@ -1,12 +1,12 @@
 <script lang="ts">
   import fragment from '../stores/fragment'
-    import { isMobile } from '../stores/mediaQueries'
+  import { isMobile } from '../stores/mediaQueries'
 
   export let text: string
 
   import { convertNZMGReferenceToLatLng } from '../utils/mapReference'
   const regex = /([a-zA-z]\d{0,2}\s?\d{2,3}\s?\d{2,3})/gim
-  const parts = text
+  const parts = (text ?? '')
     .split(regex)
     .map((p) => ({ latlng: convertNZMGReferenceToLatLng(p), text: p }))
 </script>
@@ -27,7 +27,7 @@
             text: part.text,
           },
           // On mobile, close the mountain view.
-          page: $isMobile ? undefined : v.page
+          page: $isMobile ? undefined : v.page,
         }))
       }}>{part.text}</a>
   {:else}
