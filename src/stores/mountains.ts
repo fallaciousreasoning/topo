575aaece05f32a38fca505bc339a7262f94d0acd
Jay Harris <jay.harris@outlook.co.nz>
2023-04-05 16:01:05 +1200

    Add typing for route

--- src/stores/mountains.ts
+++ src/stores/mountains.ts
@@ -1,9 +1,37 @@
 import { writable } from "svelte/store";
 import type MountainsJson from '../../public/data/mountains.json'
 
-export type Mountain = typeof MountainsJson[keyof typeof MountainsJson];
+export type Mountain = typeof MountainsJson[keyof typeof MountainsJson] & { routes: Route[] };
 export type Mountains = {
     [href: string]: Mountain
+}
+
+export interface Pitch {
+    ewbank: string;
+    alpine: string;
+    commitment: string;
+    mtcook: string;
+    aid: string;
+    ice: string;
+    mixed: string;
+    boulder: string;
+    length: string;
+    bolts: string;
+    trad: boolean;
+    description: string;
+}
+
+export interface Route {
+    name: string;
+    link: string;
+    image?: string;
+    grade: string;
+    pitches: Pitch[];
+    quality: number;
+    bolts: string;
+    natural_pro: boolean;
+    description: string;
+    ascent: string;
 }
 
 const createStore = () => {
