ee8d4081e7f2894eb85e7582c23fd143756d75d8
Jay Harris <jay.harris@outlook.co.nz>
2023-05-16 16:23:18 +1200

    Scroll to route

--- src/components/mountains/PlaceInfo.svelte
+++ src/components/mountains/PlaceInfo.svelte
@@ -2,7 +2,7 @@
   import type { Mountain } from '../../stores/mountains'
   import TopoText from '../TopoText.svelte'
   import RouteInfo from './RouteInfo.svelte'
-  import { scrollTo } from '../../directives/scrollTo';
+  import { scrollTo } from '../../directives/scrollTo'
 
   export let mountain: Mountain
   export let scrollToRoute: string | undefined = undefined
@@ -13,7 +13,10 @@
 </h1>
 {#if mountain.image}
   <a target="_blank" href={mountain.image} rel="noopener noreferrer">
-    <img class="w-full" alt={mountain.name} src={mountain.image} />
+    <img
+      class="h-64 w-full object-cover object-top"
+      alt={mountain.name}
+      src={mountain.image} />
   </a>
 {/if}
 {#if mountain.description}
@@ -24,8 +27,10 @@
   <span class="font-bold mt-4">Routes</span>
 
   {#each mountain.routes as route}
-  <hr class="mb-4" class:bg-gray-300={scrollToRoute === route.name} />
-    <div use:scrollTo={scrollToRoute === route.name}>
+    <hr class="mb-2"/>
+    <div
+      class={scrollToRoute === route.name ? 'bg-yellow-100 p-2 -m-2 -mx-4' : ''}
+      use:scrollTo={scrollToRoute === route.name}>
       <RouteInfo {route} />
     </div>
   {/each}
