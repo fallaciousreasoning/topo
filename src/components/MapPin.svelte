336cead27118e064ee85c4a5e7ba5fe90cf3c40e
Jay Harris <jay.harris@outlook.co.nz>
2024-01-05 15:02:24 +1300

    PoC map pin

---
+++ src/components/MapPin.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+    import { toLonLat } from 'ol/proj'
+  import { getOlContext } from '../ol/Map.svelte'
+    import fragment from '../stores/fragment'
+
+  const { map } = getOlContext()
+
+  map.on('click', e => {
+    const [lng, lat] = toLonLat(e.coordinate)
+    $fragment.label = {
+        lat,
+        lng,
+        text: `Lat/Lng: ${lat},${lng}`
+    }
+  })
+</script>
