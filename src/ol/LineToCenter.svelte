78da94494e918f75904465ef23ca0012ac7db479
Jay Harris <jay.harris@outlook.co.nz>
2024-02-05 21:15:47 +1300

    WIP line

--- src/ol/LineToCenter.svelte
+++ src/ol/LineToCenter.svelte
@@ -1,7 +1,8 @@
 <script lang="ts">
   import Feature from 'ol/Feature'
   import { type Coordinate } from 'ol/coordinate'
-  import { LineString } from 'ol/geom'
+  import { LineString, Point } from 'ol/geom'
+  import { getDistance } from 'ol/sphere'
   import { Stroke, Style, Text } from 'ol/style'
   import { getContext } from 'svelte'
   import { getOlContext } from './Map.svelte'
@@ -9,11 +10,11 @@
   export let start: Coordinate
   export let style = new Style({
     text: new Text({
-        text: "Foo",
-        stroke: new Stroke({ color: 'black'}),
-        offsetY: -4,
-        textBaseline: 'bottom',
-        placement: 'line'
+      text: 'Foo',
+      stroke: new Stroke({ color: 'black' }),
+      offsetY: -4,
+      textBaseline: 'bottom',
+      placement: 'line',
     }),
     stroke: new Stroke({
       color: 'black',
@@ -26,11 +27,31 @@
   const { map } = getOlContext()
 
   const geometry = new LineString([start, map.getView().getCenter()!])
-  const feature = new Feature({
+  const feature = new Feature<LineString>({
     geometry,
   })
 
-  $: feature.setStyle(style)
+  $: feature.setStyle(function (feature: Feature<LineString>) {
+    const coordinates = feature.getGeometry()?.getCoordinates()!
+    const end = coordinates.at(-1)!
+
+    const distance = Math.round(getDistance(coordinates[0]!, end))
+
+    return [
+      new Style({
+        geometry: new Point(end),
+        text: new Text({
+          text: `${distance}`,
+          scale: 2,
+          stroke: new Stroke({ color: 'black' }),
+          
+          placement: 'line',
+          offsetY: -10,
+        }),
+      }),
+      style,
+    ]
+  })
 
   addFeature(feature)
 
