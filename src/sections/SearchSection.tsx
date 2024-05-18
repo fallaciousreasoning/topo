9a4e6ac643128cd8286af4f3f7e0e00ed6c90d12
Jay Harris <jay.harris@outlook.co.nz>
2024-05-18 21:23:25 +1200

    Keyboard handlers

--- src/sections/SearchSection.tsx
+++ src/sections/SearchSection.tsx
@@ -3,17 +3,37 @@ import Section from './Section';
 import geocode from '../search/geocode';
 import { usePromise } from '../hooks/usePromise';
 import { useRouteUpdater } from '../routing/router';
+import { Place } from '../search/places';
 
 export default function SearchSection() {
     const updateRoute = useRouteUpdater()
     const [query, setQuery] = React.useState('')
     const { result = [] } = usePromise(() => geocode(query), [query])
+    const [selectedIndex, setSelectedIndex] = React.useState<number>()
+
+    const selectResult = (r: Place) => updateRoute({
+        lat: r.lat,
+        lon: r.lon
+    })
+
+    React.useEffect(() => {
+        setSelectedIndex(undefined)
+    }, [query])
+
     return <Section page='search' closable exact title='Search'>
-        <input type='search' className='w-full p-2 sticky top-2' value={query} onChange={e => setQuery(e.target.value)} />
-        {result.map(r => <div className='w-full px-2 py-1 hover:bg-gray-300 cursor-pointer' onClick={() => updateRoute({
-            lat: r.lat,
-            lon: r.lon
-        })}>
+        <input type='search' className='w-full p-2 sticky top-2' value={query} onKeyDown={(e) => {
+            if (!['ArrowUp', 'ArrowDown', 'Enter'].includes(e.key)) return
+            e.preventDefault()
+
+            if (e.key === 'Enter' && selectedIndex !== undefined) {
+                selectResult(result[selectedIndex])
+            } else if (e.key === 'ArrowUp') {
+                setSelectedIndex(i => !i ? result.length - 1 : i - 1)
+            } else if (e.key === 'ArrowDown') {
+                setSelectedIndex(i => i === undefined ? 0 : (i + 1) % result.length)
+            }
+        }} onChange={e => setQuery(e.target.value)} />
+        {result.map((r, i) => <div className={`w-full px-2 py-1 hover:bg-gray-300 cursor-pointer ${i === selectedIndex && 'bg-gray-200'}`} onClick={() => selectResult(r)}>
             {r.name} ({r.type})
         </div>)}
     </Section>
--- src/sections/Section.tsx
+++ src/sections/Section.tsx
@@ -7,12 +7,21 @@ interface Props {
     title: string
     closable: boolean
     exact: boolean
+    escapeCloses?: boolean
 }
 
 export default function Section(props: Props & React.PropsWithChildren) {
+    const escapeCloses = props.escapeCloses ?? true
     const updateRoute = useRouteUpdater()
+
     return <Route path={props.page} exact={props.exact}>
-        <div className="bg-gray-100 px-4 py-2 z-20 shadow h-screen max-w-md w-screen absolute left-0 top-0 max-h-screen overflow-y-auto">
+        <div onKeyDown={e => {
+            if (!escapeCloses) return
+
+            if (e.key === 'Escape') {
+                updateRoute({ page: null })
+            }
+        }} className="bg-gray-100 px-4 py-2 z-20 shadow h-screen max-w-md w-screen absolute left-0 top-0 max-h-screen overflow-y-auto">
             <h2 className="font-semibold text-lg flex gap-1">
                 {props.closable && <button onClick={e => updateRoute({ page: null })}>☰</button>}
                 {props.title}
