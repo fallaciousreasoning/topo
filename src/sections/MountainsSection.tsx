b8a7b5caa649286b96510f1d3081f991e8c99cab
Jay Harris <jay.harris@outlook.co.nz>
2024-05-20 19:18:48 +1200

    WIP mountains section

--- src/sections/MountainsSection.tsx
+++ src/sections/MountainsSection.tsx
@@ -8,16 +8,18 @@ import MountainCard from "../components/MountainCard";
 
 export default function MountainsSection() {
     const { result: mountains = [] } = usePromise(() => fetchMountains().then(r => Object.values(r)), [])
-    console.log(mountains)
     const [query, setQuery] = useState('')
-    const filteredMountains = useMemo<Mountain[]>(() => [], [])
+    const filteredMountains = useMemo<Mountain[]>(() => mountains
+        .filter(r => !query || r.name.toLowerCase().includes(query.toLowerCase())), [query])
     return <Section page="mountains" exact closable title="Mountains">
         <Input type="search" placeholder="Search for a mountain" autoFocus value={query} onChange={e => setQuery(e.target.value)} />
-        <div className="my-2">
-            (showing {filteredMountains.length} of {mountains.length} mountains)
-        </div>
-        <div className="flex flex-col gap-2 -mx-4 -mb-4 min-h-0 flex-1">
-            {mountains?.map(m => <MountainCard key={m.link} mountain={m} />)}
+        <div className="px-4">
+            <div className="my-2">
+                (showing {filteredMountains.length} of {mountains.length} mountains)
+            </div>
+            <div className="flex flex-col gap-2 -mx-4 -mb-4 min-h-0 flex-1">
+                {filteredMountains.map(m => <MountainCard key={m.link} mountain={m} />)}
+            </div>
         </div>
     </Section>
 }
