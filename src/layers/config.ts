e6239f7a7951b90d69ea8295a8f49ebf2b749bd7
Jay Harris <jay.harris@outlook.co.nz>
2024-11-03 21:29:49 +1300

    Cleanup

--- src/layers/config.ts
+++ src/layers/config.ts
@@ -1,7 +1,7 @@
-import { MapStyle } from "react-map-gl"
-
 export const LINZ_BASEMAPS_KEY = 'd01fbtg0ar3v159zx4e0ajt0n09'
 export const linzVectorId = 'topoVector'
+
+import { StyleSpecification } from "maplibre-gl"
 
 type LayerShared = {
     id: string,
@@ -13,7 +13,7 @@ type LayerShared = {
 export type BaseLayerDefinition = {
     version?: number,
     type: 'base',
-} & Pick<MapStyle, 'layers' | 'sources'> & LayerShared
+} & Pick<StyleSpecification, 'layers' | 'sources'> & LayerShared
 
 export interface OverlayDefinition extends LayerShared {
     type: 'overlay',
