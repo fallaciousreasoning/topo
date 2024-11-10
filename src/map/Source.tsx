ba1acb3f4a92cb73d422c34c89cbd13855f8df5c
Jay Harris <jay.harris@outlook.co.nz>
2024-11-10 15:24:40 +1300

    Working drawing

--- src/map/Source.tsx
+++ src/map/Source.tsx
@@ -1,4 +1,4 @@
-import { CanvasSourceSpecification, GeoJSONSourceSpecification, SourceSpecification, VectorSourceSpecification } from "maplibre-gl";
+import { CanvasSourceSpecification, GeoJSONSource, GeoJSONSourceSpecification, SourceSpecification, VectorSourceSpecification } from "maplibre-gl";
 import { useEffect } from "react";
 import { useMap } from "./Map";
 import React from "react";
@@ -39,6 +39,13 @@ export default function Source({ id, spec, children }: React.PropsWithChildren<{
             }
         }
     }, [id])
+
+    useEffect(() => {
+        if (!('data' in spec)) return
+
+        const source = map.getSource(id) as GeoJSONSource
+        source.setData(spec.data)
+    }, [spec['data']])
 
     return loaded ? children : null
 }
