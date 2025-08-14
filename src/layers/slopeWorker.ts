ca1bdcfd0b1dadef2506bf12041749faf9914cf8
Jay Harris <jay.harris@outlook.co.nz>
2025-08-14 16:06:12 +1200

    Add DOM and WebWorker libs

--- src/layers/slopeWorker.ts
+++ src/layers/slopeWorker.ts
@@ -44,10 +44,9 @@ const colorStops = [
     { angle: 0, color: { r: 0, g: 0, b: 0, a: 0 } },        // Transparent
     { angle: 20, color: { r: 0, g: 0, b: 0, a: 0 } },        // Transparent
     { angle: 30, color: { r: 255, g: 255, b: 0, a: 255 } }, // Yellow
-    { angle: 40, color: { r: 255, g: 165, b: 0, a: 255 } }, // Orange
-    { angle: 50, color: { r: 255, g: 0, b: 0, a: 255 } },   // Red
-    { angle: 60, color: { r: 128, g: 0, b: 128, a: 255 } }, // Purple
-    { angle: 70, color: { r: 0, g: 0, b: 0, a: 255 } }      // Black
+    { angle: 40, color: { r: 255, g: 0, b: 0, a: 255 } },   // Red
+    { angle: 50, color: { r: 128, g: 0, b: 128, a: 255 } }, // Purple
+    { angle: 60, color: { r: 0, g: 0, b: 0, a: 255 } }      // Black
 ]
 
 // Pending tile requests waiting for elevation data
